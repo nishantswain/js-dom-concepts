c810a7c122c72d5cf064ffc656a692b25fb8d782
unknown <nishantswain15@gmail.com>
2022-05-08 22:49:31 +0530

    nodes and cloneNode()

--- app.js
+++ app.js
@@ -1,17 +1,9 @@
-let bookNames = document.querySelectorAll('#book-list li .name')
-console.log(bookNames)
+const banner = document.querySelector('#page-banner');
 
-//Accessing list of elements and consoling their text-content
-bookNames.forEach((bookName) => {
-    console.log(bookName.textContent)
-})
+console.log('#page-banner node type is:', banner.nodeType);
+console.log('#page-banner node name is:', banner.nodeName);
+console.log('#page-banner has child nodes:', banner.hasChildNodes());
 
-//Changing the textContent
-bookNames.forEach((bookName) => {
-    bookName.textContent += ' myCustomText'
-})
-
-//appending to existing innerHtml
-let bookList = document.querySelector('#book-list')
-console.log(bookList.innerHTML)
-bookList.innerHTML += '<p>Nishant</p>'
+//Passing the true value here, creates a deep copy along with child nodes
+const clonedBanner = banner.cloneNode(true);
+console.log(clonedBanner.childNodes);
